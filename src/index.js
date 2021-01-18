a9d961c7e878acbdd0a7aa855ed81bf4722dc05a
lorena-1204 <mlrc1204@gmail.com>
2021-01-17 19:04:58 -0500

    Primera maquetación

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
+const getData = function(){
+    //value:devuelve un array cuyos elementos son valores de propiedades enumarables que se encuentran en el objeto. El orden de las propiedades es el mismo que el dado cuando se recorre el objeto de forma manual.
+    let cardNumber= document.getElementById("card-number").value;
+    let nameOnCard= document.getElementById("name-on-card").value;
+    let date= document.getElementById("date").value;
+    let cvc= document.getElementById("cvc").value;
 
+    let buttonValidar= document.getElementById("validar");
+    buttonValidar.addEventListener("click", validator.isValid(cardNumber));
+
+}
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,4 @@ const validator = {
 };
 
 export default validator;
+
